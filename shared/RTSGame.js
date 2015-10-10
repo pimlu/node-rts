d0146ab679ee23d3c7581d1c4b0e390f0dbac979
pimlu <pimlu@users.noreply.github.com>
2015-10-10 15:50:27 -0400

    start map initialization, prepare messaging/event system

--- shared/RTSGame.js
+++ shared/RTSGame.js
@@ -1,11 +1,30 @@
+var PQ;
 //has game state, no rendering
 function RTSGame(server) {
   this.nodes = null;
+  this.queue = new PriorityQueue({
+    comparator: function(a, b) {
+      return a - b;
+    }
+  });
 }
-if(RTSGBL.isNode) global.RTSGame = RTSGame;
 
-RTSGame.prototype.init = function(nodes) {
-  
+if(RTSGBL.isNode) {
+  global.RTSGame = RTSGame;
+  PQ = require('js-priority-queue');
+} else {
+  PQ = PriorityQueue;
+}
+
+RTSGame.prototype.init = function(nodes, w, h) {
+  this.nodes = [];
+  for(var i=0; i<nodes; i++) {
+    this.nodes.push(new Node(i,
+      Math.random()*w, Math.random()*h,
+      30, //TODO placeholder
+      i < 2 ? i+1 : 0 //2 players, rest unowned
+    ));
+  }
 };
 
 //receives an amount of time it steps by
@@ -14,10 +33,10 @@ RTSGame.prototype.step = function(dt) {
 };
 
 //loads a state
-RTSGame.prototype.readState = function(state) {
+RTSGame.prototype.loadState = function(state) {
   
 };
 
-RTSGame.prototype.readMessage = function(source, msg) {
+RTSGame.prototype.doEvent = function(source, msg) {
   
 };
--- shared/RTSNode.js
+++ shared/RTSNode.js
@@ -1,9 +1,9 @@
 
-function RTSNode(id, x, y, size, pop, owner) {
+function RTSNode(id, x, y, size, owner) {
   this.id = id;
   this.x = x; this.y = y;
   this.size = size;
-  this.pop = pop;
+  this.pop = this.maxPop() * (owner ? 1/4 : Math.random());
   this.owner = owner;
   this.attacks = [];
   this.incoming = [];
@@ -19,4 +19,13 @@ RTSNode.prototype.attack = function(id) {
     dist: 0,
     receding: false
   })
+};
+
+
+RTSNode.prototype.getGrow = function() {
+  return this.size/5+this.pop/10;
+};
+
+RTSNode.prototype.maxPop = function() {
+  return 50+Math.ceil(Math.pow(this.size, 1.2));
 };
